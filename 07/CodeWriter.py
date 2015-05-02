e1c56c1408037bf337bf580ac45a38c7eacf9a6d
David Ariel <boogalla@gmail.com>
2015-05-02 17:40:55 +0300

    small fixes

--- 07/CodeWriter.py
+++ 07/CodeWriter.py
@@ -21,20 +21,20 @@ class CodeWriter(object):
         self._output_file = open(output_file_path, "w")
         self._file_name = os.path.basename(os.path.splitext(output_file_path)[0])
         self._a_instruction(str(STACK_BASE_ADDRESS))
-        self._c_instruction("D", "A", None)
+        self._c_instruction("D", "A")
         self._set_var("SP")
         self._label_count = 1
 
     #setting a variable with name
     def _set_var(self, symbol):
         self._a_instruction(symbol)
-        self._c_instruction("M", "D", None)
+        self._c_instruction("M", "D")
 
     #write a instruction to this adress
     def _a_instruction(self, address):
         self._output_file.write("@" + address + "\n")
 
-    def _c_instruction(self, dest, comp, jump):
+    def _c_instruction(self, dest, comp, jump = None):
         if dest is not None:
             self._output_file.write(dest + "=")
         self._output_file.write(comp)
@@ -48,28 +48,28 @@ class CodeWriter(object):
     #retrieving wht in SP to A
     def _get_sp(self):
         self._a_instruction("SP")
-        self._c_instruction("A", "M", None)
+        self._c_instruction("A", "M")
 
     #popping from stack to dest
     def _pop_to_dest(self, dest):
         self._a_instruction("SP")
-        self._c_instruction("M", "M-1", None)
+        self._c_instruction("M", "M-1")
         self._get_sp()
-        self._c_instruction(dest, "M", None)
+        self._c_instruction(dest, "M")
 
     #pushing from dest to stack
     def _push_dest(self, dest):
         self._get_sp()
-        self._c_instruction("M", dest, None)
+        self._c_instruction("M", dest)
         self._a_instruction("SP")
-        self._c_instruction("M", "M+1", None)
+        self._c_instruction("M", "M+1")
 
     #comands using two argument
     def _binary_command(self, action):
         self._output_file.write("\n")
         self._pop_to_dest("D")
         self._pop_to_dest("A")
-        self._c_instruction("D", "A" + action + "D", None)
+        self._c_instruction("D", "A" + action + "D")
         self._push_dest("D")
         self._output_file.write("\n")
 
@@ -77,7 +77,7 @@ class CodeWriter(object):
     def _unary_command(self, comp):
         self._output_file.write("\n")
         self._pop_to_dest("A")
-        self._c_instruction("D", comp + "A", None)
+        self._c_instruction("D", comp + "A")
         self._push_dest("D")
         self._output_file.write("\n")
 
@@ -94,12 +94,12 @@ class CodeWriter(object):
         self._pop_to_dest("D")
         self._pop_to_dest("A")
         #checking if the signs are different
-        self._c_instruction("D", "A-D", None) # putting their diference in D
+        self._c_instruction("D", "A-D") # putting their diference in D
         self._push_dest("-1")
         true_jump_lable = self._jump("D", action)   #jump to true label if true
         #else does the following code-putting 0 in the stack
         self._a_instruction("SP")
-        self._c_instruction("M", "M-1", None)
+        self._c_instruction("M", "M-1")
         self._push_dest("0")
         self._w_label(true_jump_lable)    #true label where to skip to
         self._output_file.write("\n")
@@ -124,17 +124,17 @@ class CodeWriter(object):
     #a coomon between some push commands
     def _push_regs(self, segment, index):
         self._a_instruction(segment)
-        self._c_instruction("D", "M", None)
+        self._c_instruction("D", "M")
         self._a_instruction(str(index))
-        self._c_instruction("A", "A+D", None)
-        self._c_instruction("D", "M", None)
+        self._c_instruction("A", "A+D")
+        self._c_instruction("D", "M")
 
     #a coomon between some pop commands
     def _pop_regs(self, segment, index):
         self._a_instruction(segment)
-        self._c_instruction("D", "M", None)
+        self._c_instruction("D", "M")
         self._a_instruction(str(index))
-        self._c_instruction("D", "A+D", None)
+        self._c_instruction("D", "A+D")
         self._set_var("R"+str(REG))
 
     #by the given command, segment, index call the correct writer function
@@ -147,20 +147,20 @@ class CodeWriter(object):
                 self._push_regs(str(LCL), index)
             elif segment == "static":
                 self._a_instruction(self._file_name + str(index))
-                self._c_instruction("D", "M", None)
+                self._c_instruction("D", "M")
             elif segment == "constant":
                 self._a_instruction(str(index))
-                self._c_instruction("D", "A", None)
+                self._c_instruction("D", "A")
             elif segment == "this":
                 self._push_regs(str(THIS), index)
             elif segment == "that":
                 self._push_regs(str(THAT), index)
             elif segment == "pointer":
                 self._a_instruction(str(THIS + index))
-                self._c_instruction("D", "M", None)
+                self._c_instruction("D", "M")
             elif segment == "temp":
                 self._a_instruction(str(TEMP + index))
-                self._c_instruction("D", "M", None)
+                self._c_instruction("D", "M")
             self._push_dest("D")
         elif command == "pop":
             if segment == "argument":
@@ -169,7 +169,7 @@ class CodeWriter(object):
                 self._pop_regs(str(LCL), str(index))
             elif segment == "static":
                 self._a_instruction(self._file_name+str(index))
-                self._c_instruction("D", "A", None)
+                self._c_instruction("D", "A")
                 self._set_var("R"+str(REG))
             elif segment == "this":
                 self._pop_regs(str(THIS), str(index))
@@ -177,16 +177,16 @@ class CodeWriter(object):
                 self._pop_regs(str(THAT), str(index))
             elif segment == "pointer":
                 self._a_instruction(str(THIS + index))
-                self._c_instruction("D", "A", None)
+                self._c_instruction("D", "A")
                 self._set_var("R"+str(REG))
             elif segment == "temp":
                 self._a_instruction(str(TEMP + index))
-                self._c_instruction("D", "A", None)
+                self._c_instruction("D", "A")
                 self._set_var("R"+str(REG))
             self._pop_to_dest("D")
             self._a_instruction("R"+str(REG))
-            self._c_instruction("A", "M", None)
-            self._c_instruction("M", "D", None)
+            self._c_instruction("A", "M")
+            self._c_instruction("M", "D")
         self._output_file.write("\n")
 
     #closing the file we written
